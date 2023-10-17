ed88416fa11e184245c428eb736a24aca951d2bd
YuShifan <894402575bt@gmail.com>
2023-10-17 17:07:07 +0800

    feat(desktop): update eslint config

--- packages/eslint-config-custom/desktop.js
+++ packages/eslint-config-custom/desktop.js
@@ -1,20 +1,4 @@
 module.exports = {
-  extends: [
-    'eslint:recommended',
-    'plugin:vue/vue3-recommended',
-    '@electron-toolkit',
-    '@electron-toolkit/eslint-config-ts/eslint-recommended',
-    '@vue/eslint-config-typescript/recommended',
-    '@vue/eslint-config-prettier',
-    'plugin:prettier/recommended',
-  ],
-  rules: {
-    'vue/require-default-prop': 'off',
-    'vue/multi-word-component-names': 'off',
-    'prettier/prettier': ['warn', {}, { usePrettierrc: true }],
-  },
-  plugins: ['prettier'],
-  parserOptions: {
-    parser: '@typescript-eslint/parser',
-  },
+  root: true,
+  extends: ['./common.js', '@electron-toolkit', '@electron-toolkit/eslint-config-ts/eslint-recommended'],
 }
